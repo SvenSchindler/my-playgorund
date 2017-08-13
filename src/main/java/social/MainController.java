9299e0d47d48859a91ec67bcb6dd82bc44a336f5
Sven Schindler <sschindl@uni-potsdam.de>
2017-08-13 11:52:45 +0200

    adds main controller

---
+++ src/main/java/social/MainController.java
@@ -0,0 +1,14 @@
+package social;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Main Controller";
+    }
+
+}
