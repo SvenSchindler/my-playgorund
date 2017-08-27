78d85139060104c87e31892c62e816bb8e0622ca
Sven Schindler <sschindl@uni-potsdam.de>
2017-08-27 18:26:20 +0200

    adds configurable upload path

--- src/main/java/social/storage/StorageServiceImpl.java
+++ src/main/java/social/storage/StorageServiceImpl.java
@@ -1,6 +1,7 @@
 package social.storage;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,11 +10,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 @Component
-public class StorageServiceImpl implements StorageService{
+public class StorageServiceImpl implements StorageService {
+
+    @Value("${uploads.images.path}")
+    private String uploadPath;
+
     @Override
     public void store(MultipartFile file) {
         try {
-            Files.copy(file.getInputStream(), new File("/tmp/upload").toPath());
+            Files.copy(file.getInputStream(), new File(uploadPath + "/" + file.getOriginalFilename()).toPath());
         } catch (IOException e) {
             e.printStackTrace();
         }
